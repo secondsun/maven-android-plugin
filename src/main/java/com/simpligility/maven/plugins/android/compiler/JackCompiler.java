757e9d1703359d9580a667c9501726bde9ec05b0
Summers Pittman <secondsun@gmail.com>
2016-10-05 08:31:30 -0400

    JackCompiler was based on CSharCompiler.  Adding the correct licensing

--- src/main/java/com/simpligility/maven/plugins/android/compiler/JackCompiler.java
+++ src/main/java/com/simpligility/maven/plugins/android/compiler/JackCompiler.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * based on org.codehaus.plexus.compiler.csharp.CSharpCompiler
+ *  https://goo.gl/IrOgTG
+ *
+ */
+
 package com.simpligility.maven.plugins.android.compiler;
 
 import com.google.api.client.repackaged.com.google.common.base.Strings;
@@ -52,6 +72,7 @@ public class JackCompiler extends AbstractCompiler
     public String[] createCommandLine( CompilerConfiguration cc ) throws CompilerException
     {
         String androidHome = System.getenv( "ANDROID_HOME" );
+        
         String jackJarPath = androidHome + "/build-tools/24.0.2/jack.jar";
         String androidJarPath = androidHome + "/platforms/android-24/android.jar";
         String command =
